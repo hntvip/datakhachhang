8197a9d3c525c6d6a2af87435bf3ac0eb493dc91
Tai Phan <tai@sensolus.com>
2019-05-03 17:56:09 +0700

    add cors module that connect client from CORS

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const db = require('./db');
 
 // replace the uri string with your connection string.
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
