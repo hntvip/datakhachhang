5ce525f202099dfdf984b9539231d6bb2179b858
Tai Phan <tai@sensolus.com>
2019-05-23 15:49:31 +0700

    move container from home component to app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,78 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/don-hang">Danh Sach Khach Hang</router-link>
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container style="border: 2px solid red">
+      <!-- header -->
+      <el-header style="height:120px; border:2px solid blue">
+        <img class="logo-vue" alt="Vue logo" width="50px" height="50px" src="./assets/logo.png">
+        <div id="nav">
+          <router-link to="/">Home</router-link> | 
+          <router-link to="/don-hang">Danh Sach Khach Hang</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+      </el-header>
+      <!-- end header -->
+      <el-container>
+        <el-aside :style="{ width: changeWidth + 'px' }">
+          <el-button 
+            type="primary" 
+            :icon="setIconCollapseMenu" 
+            @click="toggle"
+          >
+          </el-button>
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            :collapse="isCollapse"
+            :router="true"
+          >
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span slot="title">Đơn hàng</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="/don-hang/create">Tạo đơn hàng</el-menu-item>
+                <el-menu-item index="/don-hang">Danh sách đơn hang</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span slot="title">Khách hàng</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="2-1">Danh Sách Khách Hàng</el-menu-item>
+                <el-menu-item index="1-2">Biểu đồ thống kê</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+            <el-menu-item index="about">
+              <i class="el-icon-document"></i>
+              <span slot="title">About</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main style="height:700px; border: 2px solid green">
+          <router-view/>
+        </el-main>
+      </el-container>
+      <el-footer style="border: 2px solid green">
+        <p>Copyright by Tu</p>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
@@ -27,4 +81,43 @@
     }
   }
 }
+.logo-vue {
+  margin:10px;
+}
 </style>
+<script>
+export default {
+  name: "app",
+  components: {
+
+  },
+  data() {
+    return {
+      isCollapse: false
+    };
+  },
+  computed: {
+    changeWidth() {
+      if (this.isCollapse) {
+        return 65;
+      } else {
+        return 300;
+      }
+    },
+    setIconCollapseMenu() {
+      return this.isCollapse ? 'el-icon-arrow-right' : 'el-icon-arrow-left';
+    }
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    toggle() {
+      this.isCollapse = !this.isCollapse;
+    }
+  }
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,52 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" width="50px" height="50px" src="../assets/logo.png">
-    <HelloWorld msg="VUEJS"/>
+  <div class="home">    
+    <HelloWorld msg="Hello"/>
 
-    <el-container style="height:100%; border: 2px solid red">
-      <!-- header -->
-      <el-header >Header content</el-header>
-      <!-- end header -->
-      <el-container>
-        <el-aside :style="{ width: changeWidth + 'px' }" style="border: 5px solid black">
-          <el-button type="primary" icon="el-icon-edit" @click="toggle"></el-button>
-          <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose"
-            :collapse="isCollapse"
-          >
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span slot="title">Đơn hàng</span>
-              </template>
-              <el-menu-item-group>
-                <el-menu-item index="1-1">Tạo đơn Hàng</el-menu-item>
-                <el-menu-item index="1-2">Đơn hang mới nhất</el-menu-item>
-              </el-menu-item-group>
-            </el-submenu>
-            <el-submenu index="2">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span slot="title">Khách hàng</span>
-              </template>
-              <el-menu-item-group>
-                <el-menu-item index="2-1">Danh Sách Khách Hàng</el-menu-item>
-                <el-menu-item index="1-2">Biểu đồ thống kê</el-menu-item>
-              </el-menu-item-group>
-            </el-submenu>
-            <el-menu-item index="3">
-              <i class="el-icon-document"></i>
-              <span slot="title">About</span>
-            </el-menu-item>
-          </el-menu>
-        </el-aside>
-        <el-main style="border: 2px solid green">Main</el-main>
-      </el-container>
-      <el-footer style="border: 2px solid green">footer of page</el-footer>
-    </el-container>
   </div>
 </template>
 
@@ -58,31 +13,6 @@ export default {
   name: "home",
   components: {
     HelloWorld
-  },
-  data() {
-    return {
-      isCollapse: false
-    };
-  },
-  computed: {
-    changeWidth() {
-      if(this.isCollapse){
-        return 55;
-      }else {
-        return 300;
-      }
-    }
-  }, 
-  methods: {
-    handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-    },
-    handleClose(key, keyPath) {
-      console.log(key, keyPath);
-    },
-    toggle() {
-      this.isCollapse = !this.isCollapse
-    }
   }
 };
 </script>
