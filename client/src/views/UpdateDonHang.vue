9afc5f85b5aeb851c722877625359b16e8bf4714
Tu Huynh <harrypottergirl93@gmail.com>
2019-05-23 21:51:57 +0700

    change validate form

--- client/src/views/UpdateDonHang.vue
+++ client/src/views/UpdateDonHang.vue
@@ -3,23 +3,23 @@
     <el-row type="flex" class="row-bg" justify="center">
       <el-col :span="12">
         <div class="grid-content bg-purple-light">
-          <el-form :model="form" ref="form" label-width="180px">
-            <el-form-item label="Nhap Ma Don Hang" prop="orderNumber" error="Phai Nhap Ma Don Hang" required>
+          <el-form :model="form" :rules="rules" ref="form" label-width="200px">
+            <el-form-item label="Nhap Ma Don Hang" prop="orderNumber">
                 <el-col :span="12">
                      <el-input v-model="form.orderNumber"></el-input>
                 </el-col>
             </el-form-item>
-            <el-form-item label="Nhap So Dien Thoai Khach" error="Phai Nhap SDT khach"  prop="phoneNumber" required>
+            <el-form-item label="Nhap So Dien Thoai Khach" prop="phoneNumber">
                 <el-col :span="12">
                     <el-input v-model="form.phoneNumber"></el-input>
                 </el-col>
             </el-form-item>
-            <el-form-item label="Nhap Dia Chi (Tinh/TP)" error="Phai Nhap DIA CHI" prop="address" required>
+            <el-form-item label="Nhap Dia Chi (Tinh/TP)" prop="address">
                 <el-col :span="12">
                   <el-input v-model="form.address"></el-input>
                 </el-col>
             </el-form-item>
-            <el-form-item label="Nhap Shop Ban Hang" error="Phai Nhap SHOP" prop="shopID" required>
+            <el-form-item label="Nhap Shop Ban Hang" prop="shopID">
                 <el-col :span="12">
                     <el-select
                         v-model="form.shopID"
@@ -37,7 +37,7 @@
             </el-form-item>
 
             <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')">Submit</el-button>
+              <el-button type="primary" @click="submitForm('form')">Submit</el-button>
               <el-button @click="resetForm('form')">Xoa Form Lam Lai</el-button>
             </el-form-item>
           </el-form>
@@ -60,6 +60,24 @@ export default {
         orderNumber: '',
         shopID: '',
         phoneNumber:''
+      },
+      rules:{
+        name:[
+          { required: true, message: 'Bạn nhập thiếu tên', trigger: 'blur' },
+          { min: 3, max: 100, message: 'Bạn nhập tối thiểu 3 ký tự', trigger: 'blur' }
+        ],
+        orderNumber:[
+          { required: true, message: 'Bạn nhập thiếu mã đơn hàng', trigger: 'blur' }
+        ],
+        phoneNumber:[
+          { required: true, message: 'Bạn nhập thiếu sdt', trigger: 'blur' }
+        ],
+        address:[
+          { required: true, message: 'Bạn nhập thiếu sdt', trigger: 'blur' }
+        ],
+        shopID:[
+          { required: true, message: 'Bạn nhập thiếu sdt', trigger: 'blur' }
+        ],
       }
     };
   },
